9226503d482c93eaeb8f437494e2e9366bd649be
eris6 <erispatel20@gmail.com>
2024-11-04 22:20:10 -0500

    create gameboard class

--- script.js
+++ script.js
@@ -21,14 +21,17 @@ function Gameboard(){
         return;
     }
 
+    const checkWin = () => {
+        const winnerCoordinates = []
+    }
+
     const printBoard = () =>{
         const boardValues = board.map((row) => row.map((cell) => cell.getValue()));
         console.log(boardValues);
     }
         
-    return {getBoard, markBoard, printBoard};
+    return {getBoard, markBoard, checkWin, printBoard};
 }
-
 
 function Square(){
     let value = ' ';
@@ -41,3 +44,71 @@ function Square(){
     return {markSquare, getValue};
 }
 
+function Player(){
+    let playerName = ' ';
+    let playerMarker = ' ';
+
+    const getPlayerName = () => playerName;
+    const getMarker = () => playerMarker;
+
+    const setPlayerName = (newName) => {
+        playerName = newName;
+    }
+
+    const setMarker = (newMarker) =>{
+        playerMarker = newMarker;
+    }
+
+    return {getPlayerName, getMarker, setPlayerName, setMarker};
+}
+
+function GameController(playerOneName = "Player One", playerTwoName = "Player Two"){
+    let playerOne = Player();
+    playerOne.setMarker('X');
+    playerOne.setPlayerName(playerOneName);
+    let playerTwo = Player();
+    playerTwo.setMarker('O');
+    playerTwo.setPlayerName(playerTwoName);
+
+    const players = [playerOne, playerTwo];
+    let board = Gameboard();
+
+    const getPlayers = () => {
+        players.forEach((player) => {
+            console.log(player.getPlayerName());
+            console.log(player.getMarker());
+        })
+    };
+
+    let activePlayer = players[0];
+
+    const switchTurn = () =>{
+        if (activePlayer == players[0]){activePlayer = players[1];}
+        else {activePlayer = players[0];}
+    }
+
+    const getActivePlayer = () => activePlayer.getPlayerName();
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer()}'s turn`)
+    }
+
+
+    const makeMove = (row, col) => {
+        let cell = board.getBoard()[row][col];
+        if (cell.getValue() === ' '){
+            board.markBoard(row, col, activePlayer.getMarker());
+            switchTurn();
+        }
+
+
+    }
+
+    return {getPlayers, switchTurn, getActivePlayer, printNewRound, makeMove};
+}
+
+
+let game = Gameboard();
+game.markBoard(1, 0, "X");
+game.printBoard();
